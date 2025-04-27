4bd3eab6cd24d0ec41191b91acfb563af660e237
memradio <volodymyr.y.kovalenko@gmail.com>
2025-04-27 10:44:53 +0200

    Create memdata.js

---
+++ memdata.js
@@ -0,0 +1,475 @@
+  // JSON-дані
+    let memeData = 
+[
+  {
+    number: "001",
+    name: "Маниш мене дурманиш",
+    description: "Фраза з українського тіктоку, популярного у 2023 році — жінка емоційно повторює слова пісні.",
+    youtubelink: "https://www.youtube.com/watch?v=VYTXc-E53gk"
+  },
+  {
+    number: "002",
+    name: "отвали со свои вот етим самим",
+    description: "Фраза з тіктоку — жінка з емоціями відганяє співрозмовника.",
+    youtubelink: "https://www.youtube.com/watch?v=4aIIdgr-lvs"
+  },
+  {
+    number: "003",
+    name: "шо воно казать еслі канешно",
+    description: "Не дуже поширений, скоріше локальний мем — джерело невідоме.",
+    youtubelink: ""
+  },
+  {
+    number: "004",
+    name: "Я в ахуе давай",
+    description: "Фраза з відео, де хлопець в шоці і каже 'я в ахуе, давай', дуже популярний серед українських коротких відео.",
+    youtubelink: "https://www.youtube.com/shorts/LFslFyV0qx0"
+  },
+  {
+    number: "005",
+    name: "no well",
+    description: "Кліп з YouTube Shorts або TikTok, де людина каже 'No... well...' і міняє думку — мем про нерішучість.",
+    youtubelink: "https://www.youtube.com/shorts/mElSTk-EayY"
+  },
+  {
+    number: "006",
+    name: "stop spending your money on stupid shit",
+    description: "Мем з TikTok — порада, яку часто беруть іронічно. Використовується для висміювання імпульсивних витрат.",
+    youtubelink: "https://www.youtube.com/watch?v=BgGUo-VJSuM"
+  },
+  {
+    number: "007",
+    name: "what do you mean",
+    description: "Часто використовується у форматі реакцій на щось незрозуміле. Є кілька варіантів мема, джерело розмите.",
+    youtubelink: ""
+  },
+  {
+    number: "008",
+    name: "Марина",
+    description: "Мем із жіночого голосу, що емоційно вигукує 'Марина!'. Часто використовується у відео з несподіваним розвитком.",
+    youtubelink: "https://www.youtube.com/watch?v=1amgLEV-FA8"
+  },
+  {
+    number: "009",
+    name: "вечно надута",
+    description: "Фраза з побутової лайф-ситуації, де жінка скаржиться на когось — часто використовують у гумористичних монтажах.",
+    youtubelink: "https://www.youtube.com/watch?v=peGwy2qhtXI"
+  },
+  {
+    number: "010",
+    name: "Добрий вечір ведющій",
+    description: "Не знаю",
+    youtubelink: ""
+  },
+  {
+    "number": "011",
+    "name": "Как-то все не побагатому",
+    "description": "Фраза з українського тіктоку, популярна у 2024 році — жінка емоційно каже 'как-то все не побагатому'.",
+    "youtubelink": "https://www.youtube.com/shorts/qIGfOF8ldeQ"
+  },
+  {
+    "number": "012",
+    "name": "Нехуй заняться",
+    "description": "Копіпаста з гумористичним описом бездіяльності, популярна в російськомовному інтернеті.",
+    "youtubelink": "https://www.youtube.com/watch?v=XeiSDSyUA9I"
+  },
+  {
+    "number": "013",
+    "name": "Surprise surprise",
+    "description": "Мем з британської телеведучої Сілли Блек, де вона співає 'Surprise surprise'.",
+    "youtubelink": "https://www.youtube.com/watch?v=dO8feY_GkG4"
+  },
+  {
+    "number": "014",
+    "name": "Притармажуй",
+    "description": "Мем з українського тіктоку, де людина каже 'притармажуй'.",
+    "youtubelink": "https://www.youtube.com/watch?v=0NzaNVb16bo"
+  },
+  {
+    "number": "015",
+    "name": "Нашо сохнути за тим довбойобом",
+    "description": "Фраза з українського тіктоку, де жінка емоційно каже 'нашо сохнути за тим довбойобом'.",
+    "youtubelink": "https://www.youtube.com/watch?v=1amgLEV-FA8"
+  },
+  {
+    "number": "016",
+    "name": "Мені нравицця дивиться як воно горить",
+    "description": "Мем з українського інтернету, де людина каже 'мені нравицця дивиться як воно горить'.",
+    "youtubelink": "https://www.youtube.com/watch?v=IswfPZRVq8c"
+  },
+  {
+    "number": "017",
+    "name": "Похуй похуй похуй",
+    "description": "Мем з російського інтернету, де людина каже 'похуй похуй похуй'.",
+    "youtubelink": "https://www.youtube.com/watch?v=7gkxO1bmiwk"
+  },
+  {
+    "number": "018",
+    "name": "Собака рже",
+    "description": "Мем з відео, де собака сміється, популярний у російськомовному інтернеті.",
+    "youtubelink": "https://www.youtube.com/watch?v=eJT1PuBgDto"
+  },
+  {
+    "number": "019",
+    "name": "Спасибо дура",
+    "description": "Мем з російського інтернету, де людина каже 'спасибо дура'.",
+    "youtubelink": "https://www.youtube.com/watch?v=zeg1VKMNrH8"
+  },
+  {
+    "number": "020",
+    "name": "Фамилия имя отчество",
+    "description": "Мем з російського інтернету, де людина каже 'фамилия имя отчество'.",
+    "youtubelink": "https://www.youtube.com/watch?v=KxQO8uyQ5TI"
+  },
+  {
+    "number": "021",
+    "name": "Шариш в этой теме",
+    "description": "Фраза з російського інтернету, де людина запитує: 'Шариш в этой теме?' — популярна у відео з TikTok.",
+    "youtubelink": "https://www.youtube.com/watch?v=Z30tOaVsTfY"
+  },
+  { "number": "022", "name": "я хочу беляяяяяяяш" },
+  { "number": "023", "name": "Шо то типа обед будет" },
+  { "number": "024", "name": "є стіл є кресло" },
+  { "number": "025", "name": "пойми дура в мене живіт болить" },
+  { "number": "026", "name": "Діма ти не дома" },
+  { "number": "027", "name": "Зубожиня" },
+  { "number": "028", "name": "там така компанія зіпєшся і піздець тобі" },
+  { "number": "029", "name": "Думает, что любит тебя!! оригинал" },
+  { "number": "030", "name": "ну вас всех в пизду" },
+  {
+    "number": "031",
+    "name": "ну й ш ну й шо",
+    "description": "Фраза з українського інтернету, популярна у відео з TikTok.",
+    "youtubelink": "https://www.youtube.com/watch?v=zoP25GgQdao"
+  },
+  {
+    "number": "032",
+    "name": "девять десять",
+    "description": "Фраза з російського інтернету, де людина каже: 'Девять десять'.",
+    "youtubelink": "https://www.youtube.com/watch?v=bNsxVa5QZMc"
+  },
+  {
+    "number": "033",
+    "name": "блядь аткуда ана взялась",
+    "description": "Фраза з російського інтернету, де людина каже: 'Блядь аткуда ана взялась'.",
+    "youtubelink": "https://www.youtube.com/watch?v=sw2WAfKOzFg"
+  },
+  {
+    "number": "034",
+    "name": "Блядь блядь сука блядь коротка версія",
+    "description": "Фраза з російського інтернету, де людина каже: 'Блядь блядь сука блядь коротка версія'.",
+    "youtubelink": "https://www.youtube.com/watch?v=sw2WAfKOzFg"
+  },
+  {
+    "number": "035",
+    "name": "блядь ти шо сука дибіл",
+    "description": "Фраза з російського інтернету, де людина каже: 'Блядь ти шо сука дибіл'.",
+    "youtubelink": "https://www.youtube.com/watch?v=sw2WAfKOzFg"
+  },
+  {
+    "number": "036",
+    "name": "ой блять та ну",
+    "description": "Фраза з українського інтернету, де людина каже: 'Ой блять та ну'.",
+    "youtubelink": "https://www.youtube.com/watch?v=jW-gOh6hcj0"
+  },
+  {
+    "number": "037",
+    "name": "та блять йомайо",
+    "description": "Фраза з російського інтернету, де людина каже: 'Та блять йомайо'.",
+    "youtubelink": "https://www.youtube.com/watch?v=sznE8kKwqnY"
+  },
+  {
+    "number": "038",
+    "name": "ти шо охуел ска",
+    "description": "Фраза з російського інтернету, де людина каже: 'Ти шо охуел ска'.",
+    "youtubelink": "https://www.youtube.com/watch?v=99h0AK6kFDY"
+  },
+  {
+    "number": "039",
+    "name": "чиназес",
+    "description": "Мем з російського інтернету, де людина каже: 'Чиназес'.",
+    "youtubelink": "https://www.youtube.com/watch?v=9qvT1z78YKA"
+  },
+  {
+    "number": "040",
+    "name": "ухти ж йоханий бабай",
+    "description": "Фраза з українського інтернету, де людина каже: 'Ухти ж йоханий бабай'.",
+    "youtubelink": "https://www.youtube.com/watch?v=sw2WAfKOzFg"
+  },
+    {
+      "number": "041",
+      "name": "Хахалі Каті",
+      "links": {
+        "tiktok": "https://www.tiktok.com/%40sidorenko_hair/video/7488336968342916357?utm_source=chatgpt.com",
+        "youtube": "https://www.youtube.com/watch?v=2843nS87Kis&utm_source=chatgpt.com"
+      }
+    },
+    {
+      "number": "042",
+      "name": "так і не скажеш шо шльондра",
+      "links": {
+        "tiktok": "https://www.tiktok.com/%40veronica_tishkova/video/7082763654361304326?utm_source=chatgpt.com",
+        "youtube": "https://www.youtube.com/watch?v=2843nS87Kis&utm_source=chatgpt.com"
+      }
+    },
+    {
+      "number": "043",
+      "name": "так мене 12 числа вже в києві не буде",
+      "links": {
+        "tiktok": "https://www.tiktok.com/@nadiastupak/video/7251211720746749190",
+        "youtube": "https://www.youtube.com/watch?v=2843nS87Kis&utm_source=chatgpt.com"
+      }
+    },
+    {
+      "number": "044",
+      "name": "сметана безхуйозна",
+      "links": {
+        "tiktok": "https://www.tiktok.com/%40taylerpremya/video/7494696807390252296?utm_source=chatgpt.com",
+        "youtube": "https://www.youtube.com/watch?v=2843nS87Kis&utm_source=chatgpt.com"
+      }
+    },
+    {
+      "number": "045",
+      "name": "сто доларов на шоби комфортно жить",
+      "links": {
+        "tiktok": "https://www.tiktok.com/%40taylerpremya/video/7494696807390252296?utm_source=chatgpt.com",
+        "youtube": "https://www.youtube.com/watch?v=2843nS87Kis&utm_source=chatgpt.com"
+      }
+    },
+    {
+      "number": "046",
+      "name": "я вас всіх понагодовую дрючком по спину",
+      "links": {
+        "tiktok": "https://www.tiktok.com/%40taylerpremya/video/7494696807390252296?utm_source=chatgpt.com",
+        "youtube": "https://www.youtube.com/watch?v=2843nS87Kis&utm_source=chatgpt.com"
+      }
+    },
+    {
+      "number": "047",
+      "name": "пиздуй із деревні",
+      "links": {
+        "tiktok": "https://www.tiktok.com/%40taylerpremya/video/7494696807390252296?utm_source=chatgpt.com",
+        "youtube": "https://www.youtube.com/watch?v=2843nS87Kis&utm_source=chatgpt.com"
+      }
+    },
+    {
+      "number": "048",
+      "name": "Світлана добрий ранок, йди нахуй",
+      "links": {
+        "tiktok": "https://www.tiktok.com/%40taylerpremya/video/7494696807390252296?utm_source=chatgpt.com",
+        "youtube": "https://www.youtube.com/watch?v=2843nS87Kis&utm_source=chatgpt.com"
+      }
+    },
+    {
+      "number": "049",
+      "name": "не умієш не берися, не кончаєш не ібися",
+      "links": {
+        "tiktok": "https://www.tiktok.com/%40rozum_znyk/video/7242186272016469254?utm_source=chatgpt.com",
+        "youtube": "https://www.youtube.com/watch?v=2843nS87Kis&utm_source=chatgpt.com"
+      }
+    },
+    {
+      "number": "050",
+      "name": "мені просто хочеться понять шо таке презент сімпл",
+      "links": {
+        "tiktok": "https://www.tiktok.com/%40vkhilya_/video/7276523683420671237?utm_source=chatgpt.com",
+        "youtube": "https://www.youtube.com/watch?v=2843nS87Kis&utm_source=chatgpt.com"
+      }
+    },  
+  { "number": "050", "name": "мені просто хочеться понять шо таке презент сімпл" },
+  { "number": "051", "name": "тепер таке время всьо курва дороге" },
+  { "number": "052", "name": "не пожила дерево не посадила" },
+  { "number": "053", "name": "как здесь класна вабще не хачу уезжать" },
+  { "number": "054", "name": "забераем свои курва монатки" },
+  { "number": "055", "name": "СМОТРЯ СКОЛЬКО DETAILS В ЭТОМ ПИДЖАКЕ" },
+  { "number": "056", "name": "слово не олівець" },
+  { "number": "057", "name": "жопа в тепле чем тепло в жопе" },
+  { "number": "058", "name": "помий мило зарубай сокиру" },
+  { "number": "059", "name": "курва нахуя я це зробив" },
+  { "number": "060", "name": "Олег ти шо плачеш" },
+  { "number": "061", "name": "Алег ти шо це плачеш" },
+  { "number": "062", "name": "я уже заебался блядт идти ска нах" },
+  { "number": "063", "name": "шаройобитись" },
+  { "number": "064", "name": "забили забули" },
+  { "number": "065", "name": "за 40 грн штани" },
+  { "number": "066", "name": "если у вас нет денег то на трасу" },
+  { "number": "067", "name": "oui oui oui" },
+  { "number": "068", "name": "не так глубоко" },
+  { "number": "069", "name": "кожну ночь перед сном я іграю с пісюнок" },
+  { "number": "070", "name": "its britney bitch short version" },
+  { "number": "071", "name": "летіла лопата" },
+  { "number": "072", "name": "i know u you know that i know u" },
+  { "number": "073", "name": "купляєм горіхи піря" },
+  { "number": "074", "name": "как у кошечки дела" },
+  { "number": "075", "name": "why are you gay full version danse" },
+  { "number": "076", "name": "Бабка ну и хуй с ним" },
+  { "number": "077", "name": "успела похудеть к лету" },
+  { "number": "078", "name": "то яма була страшна в моєму житті" },
+  { "number": "079", "name": "йди цицьку дам" },
+  { "number": "080", "name": "АЛКАШ ПОДХОДИШЬ" },
+  { "number": "081", "name": "Славка не бери молоду" },
+  { "number": "082", "name": "ББСБ повна версія" },
+  { "number": "083", "name": "hit it get it" },
+  { "number": "084", "name": "ретузіки" },
+  { "number": "085", "name": "Курган Вчителька" },
+  { "number": "086", "name": "Курган Пострижись як в прошлый раз" },
+  { "number": "087", "name": "Курган ЖОПА" },
+  { "number": "088", "name": "Курган Кровавий кулак" },
+  { "number": "089", "name": "Wellboy - Гуси" },
+  { "number": "090", "name": "Klavdia Petrivna Я щаслива" },
+  { "number": "091", "name": "VERKA SERDUCHKA - DANCING" },
+  { "number": "092", "name": "Miley Cyrus - Flowers" },
+  { "number": "093", "name": "Katy Perry - Harleys In Hawaii" },
+  { "number": "094", "name": "Лолита Шпилька-каблучок" },
+  { "number": "095", "name": "In Da Getto" },
+  { "number": "096", "name": "In Da Club" },
+  { "number": "097", "name": "The Pussycat Dolls - Don't Cha" },
+  { "number": "098", "name": "Ricky Martin - La Mordidita" },
+  { "number": "099", "name": "Якубені" },
+  { "number": "100", "name": "ILLIA HB" },
+  { "number": "101", "name": "Britney_GIVE_ME_MORE" },
+  { "number": "102", "name": "GALA - Freed from desire" },
+  { "number": "103", "name": "Грибы - Велик" },
+  { "number": "104", "name": "Britney_One_Two_Three" },
+  { "number": "105", "name": "Britney_Did_It_Again" },
+  { "number": "106", "name": "Britney Toxic" },
+  { "number": "107", "name": "Britney H" },
+  { "number": "108", "name": "Watermelon Sugar" },
+  { "number": "109", "name": "summer inside" },
+  {
+    "number": "110",
+    "name": "Doja Cat - Paint The Town Red",
+    "links": {
+      "tiktok": "https://www.tiktok.com/music/Paint-The-Town-Red-7263241548443568130",
+      "youtube": "https://www.youtube.com/watch?v=m4_9TFeMfJE"
+    }
+  },
+  {
+    "number": "110",
+    "name": "Doja Cat - Paint The Town Red",
+    "links": {
+      "tiktok": "https://www.tiktok.com/@dojafantasy/video/7263321767043714334",
+      "youtube": "https://www.youtube.com/watch?v=HXCpyHYsOfQ"
+    }
+  },
+  {
+    "number": "111",
+    "name": "Pedro",
+    "links": {
+      "tiktok": "https://www.tiktok.com/@pedrotheracoon/video/7356225255095930145",
+      "youtube": "https://www.youtube.com/watch?v=z5X0JMFJBPs"
+    }
+  },
+  {
+    "number": "112",
+    "name": "Harry Styles - Late Night Talking",
+    "links": {
+      "tiktok": "https://www.tiktok.com/@hshq/video/7119894046734208302",
+      "youtube": "https://www.youtube.com/watch?v=F2YpXC1itEE"
+    }
+  },
+  {
+    "number": "113",
+    "name": "Dua Lipa - Hallucinate",
+    "links": {
+      "tiktok": "https://www.tiktok.com/discover/dua-lipa-hallucinate",
+      "youtube": "https://www.youtube.com/watch?v=F2YpXC1itEE"
+    }
+  },
+  {
+    "number": "114",
+    "name": "Dua Lipa - Break My Heart",
+    "links": {
+      "tiktok": "https://www.tiktok.com/music/Break-My-Heart-6807639165665019906",
+      "youtube": "https://www.youtube.com/watch?v=F2YpXC1itEE"
+    }
+  },
+  {
+    "number": "115",
+    "name": "Dua Lipa - Pretty Please",
+    "links": {
+      "tiktok": "https://www.tiktok.com/discover/dua-lipa-pretty-please-video",
+      "youtube": "https://www.youtube.com/watch?v=F2YpXC1itEE"
+    }
+  },
+  {
+    "number": "116",
+    "name": "Dua Lipa - Training Season",
+    "links": {
+      "tiktok": "https://www.tiktok.com/discover/dua-lipa-training-season",
+      "youtube": "https://www.youtube.com/watch?v=F2YpXC1itEE"
+    }
+  },
+  {
+    "number": "117",
+    "name": "Dua Lipa - Houdini",
+    "links": {
+      "tiktok": "https://www.tiktok.com/@dualipaofficial/video/7299605725829942561",
+      "youtube": "https://www.youtube.com/watch?v=F2YpXC1itEE"
+    }
+  },
+  {
+    "number": "118",
+    "name": "Sophie Ellis-Bextor - Murder On The Dancefloor",
+    "links": {
+      "tiktok": "https://www.tiktok.com/music/Murder-On-The-Dancefloor-6928470186467018753",
+      "youtube": "https://www.youtube.com/watch?v=F2YpXC1itEE"
+    }
+  },
+  {
+    "number": "119",
+    "name": "Madonna - Back That Up To The Beat",
+    "links": {
+      "tiktok": "https://www.tiktok.com/discover/back-that-up-to-the-beat-madonna",
+      "youtube": "https://www.youtube.com/watch?v=F2YpXC1itEE"
+    }
+  },
+  {
+    "number": "120",
+    "name": "Enrique Iglesias - DUELE EL CORAZON",
+    "links": {
+      "tiktok": "https://www.tiktok.com/discover/enrique-iglesias-duele-el-corazon",
+      "youtube": "https://www.youtube.com/watch?v=F2YpXC1itEE"
+    }
+  },
+  {
+    "number": "121",
+    "name": "Lady Gaga - Telephone",
+    "links": {
+      "tiktok": "https://www.tiktok.com/discover/telephone-by-lady-gaga",
+      "youtube": "https://www.youtube.com/watch?v=F2YpXC1itEE"
+    }
+  },
+  {
+    "number": "122",
+    "name": "Call Me By Your Name",
+    "links": {
+      "tiktok": "https://www.tiktok.com/discover/call-me-by-your-name",
+      "youtube": "https://www.youtube.com/watch?v=F2YpXC1itEE"
+    }
+  },
+  {
+    "number": "123",
+    "name": "Bon Appetit - Katy Perry",
+    "links": {
+      "tiktok": "https://www.tiktok.com/discover/katy-perry-bon-appetite",
+      "youtube": "https://www.youtube.com/watch?v=F2YpXC1itEE"
+    }
+  },
+  {
+    "number": "124",
+    "name": "Imagine Dragons - Sharks",
+    "links": {
+      "tiktok": "https://www.tiktok.com/discover/imagine-dragons---sharks",
+      "youtube": "https://www.youtube.com/watch?v=F2YpXC1itEE"
+    }
+  },
+  {
+    "number": "125",
+    "name": "Imagine Dragons - Natural",
+    "links": {
+      "tiktok": "https://www.tiktok.com/discover/imagine-dragons-natural",
+      "youtube": "https://www.youtube.com/watch?v=F2YpXC1itEE"
+    }
+  }
+];
