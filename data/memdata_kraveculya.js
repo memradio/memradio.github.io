0b27ef41805d2a78b7891c9c8d5c48237932e8b7
Proladge <proladge@gmail.com>
2025-05-01 12:54:58 +0200

    add new mem

--- data/memdata_kraveculya.js
+++ data/memdata_kraveculya.js
@@ -11,5 +11,11 @@ window.memeData = [
       name: "002K пу пу пуу",
       audio: "002K пу пу пуу.mp3",
       links: {"tiktok": "https://www.tiktok.com/@ruslan.tsyhankov/video/7440842937665146168"}
-    }
+    },
+    {
+        number: "003К",
+        name: "003K все круто я наївся",
+        audio: "003K все круто я наївся.mp3",
+        links: {"instagram": "https://www.instagram.com/reel/DJD7eSXtNHb/?igsh=bGp6OHA1YXZhc3gw"}
+      },
   ];
