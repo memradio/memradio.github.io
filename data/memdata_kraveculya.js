c415f9b8a3fd50923e55f5d21ee06b521e13aa31
Volodymyr Kovalenko <proladge@gmail.com>
2025-05-10 21:00:19 +0200

    Update memdata_kraveculya.js

--- data/memdata_kraveculya.js
+++ data/memdata_kraveculya.js
@@ -8,14 +8,20 @@ window.memeData = [
     },
     {
       number: "002К",
-      name: "002K пу пу пуу",
+      name: "пу пу пуу",
       audio: "002K пу пу пуу.mp3",
       links: {"tiktok": "https://www.tiktok.com/@ruslan.tsyhankov/video/7440842937665146168"}
     },
     {
         number: "003К",
-        name: "003K все круто я наївся",
+        name: "все круто я наївся",
         audio: "003K все круто я наївся.mp3",
         links: {"instagram": "https://www.instagram.com/reel/DJD7eSXtNHb/?igsh=bGp6OHA1YXZhc3gw"}
+      },
+    {
+        number: "004К",
+        name: "Фереро Роше",
+        audio: "004K Фереро Роше.mp4",
+        links: {"instagram": "https://www.instagram.com/reel/DHJsiKGNzSN"}
       },
   ];
