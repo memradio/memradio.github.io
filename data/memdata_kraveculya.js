175858509f20a5f9a1b35b5d299094084e489d80
memradio <volodymyr.y.kovalenko@gmail.com>
2025-05-14 20:17:08 +0200

    додано новий мем: Djsgsksgsh

--- data/memdata_kraveculya.js
+++ data/memdata_kraveculya.js
@@ -30,4 +30,13 @@ window.memeData = [
         audio: "005K То якого фіга ти мішаєш.mp4",
         links: {"instagram": "https://www.instagram.com/reel/DIzNOFhND4l"}
     },
-  ];
+{
+  "name": "Djsgsksgsh",
+  "links": {
+    "youtube": "https://youtube.com/",
+    "tiktok": "",
+    "instagram": ""
+  },
+  "audio": ""
+}
+]
