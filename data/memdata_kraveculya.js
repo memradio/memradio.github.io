08a6bc30aa4a90b9c5569d5668fe4d0c81ea9e7f
memradio <volodymyr.y.kovalenko@gmail.com>
2025-05-14 14:31:45 +0200

    додано новий мем: test

--- data/memdata_kraveculya.js
+++ data/memdata_kraveculya.js
@@ -1,33 +1,42 @@
 window.memeData = [
     {
-      number: "001К",
-      name: "Це мій район",
+      number: "001Ð",
+      name: "Ð¦Ðµ Ð¼ÑÐ¹ ÑÐ°Ð¹Ð¾Ð½",
       description: "",
       links: {"instagram": "https://www.instagram.com/reel/DI8q-INMVi_/?igsh=NnFjczJ3cDlxNDhm"},
-      audio: "001К Це мій район.mp3"
+      audio: "001Ð Ð¦Ðµ Ð¼ÑÐ¹ ÑÐ°Ð¹Ð¾Ð½.mp3"
     },
     {
-      number: "002К",
-      name: "пу пу пуу",
-      audio: "002K пу пу пуу.mp3",
+      number: "002Ð",
+      name: "Ð¿Ñ Ð¿Ñ Ð¿ÑÑ",
+      audio: "002K Ð¿Ñ Ð¿Ñ Ð¿ÑÑ.mp3",
       links: {"tiktok": "https://www.tiktok.com/@ruslan.tsyhankov/video/7440842937665146168"}
     },
     {
-        number: "003К",
-        name: "все круто я наївся",
-        audio: "003K все круто я наївся.mp3",
+        number: "003Ð",
+        name: "Ð²ÑÐµ ÐºÑÑÑÐ¾ Ñ Ð½Ð°ÑÐ²ÑÑ",
+        audio: "003K Ð²ÑÐµ ÐºÑÑÑÐ¾ Ñ Ð½Ð°ÑÐ²ÑÑ.mp3",
         links: {"instagram": "https://www.instagram.com/reel/DJD7eSXtNHb/?igsh=bGp6OHA1YXZhc3gw"}
     },
     {
-        number: "004К",
-        name: "Фереро Роше",
-        audio: "004K Фереро Роше.mp4",
+        number: "004Ð",
+        name: "Ð¤ÐµÑÐµÑÐ¾ Ð Ð¾ÑÐµ",
+        audio: "004K Ð¤ÐµÑÐµÑÐ¾ Ð Ð¾ÑÐµ.mp4",
         links: {"instagram": "https://www.instagram.com/reel/DHJsiKGNzSN"}
     },
     {
-        number: "005К",
-        name: "То якого фіга ти мішаєш",
-        audio: "005K То якого фіга ти мішаєш.mp4",
+        number: "005Ð",
+        name: "Ð¢Ð¾ ÑÐºÐ¾Ð³Ð¾ ÑÑÐ³Ð° ÑÐ¸ Ð¼ÑÑÐ°ÑÑ",
+        audio: "005K Ð¢Ð¾ ÑÐºÐ¾Ð³Ð¾ ÑÑÐ³Ð° ÑÐ¸ Ð¼ÑÑÐ°ÑÑ.mp4",
         links: {"instagram": "https://www.instagram.com/reel/DIzNOFhND4l"}
     },
-  ];
+{
+  "name": "test",
+  "links": {
+    "youtube": "",
+    "tiktok": "",
+    "instagram": ""
+  },
+  "audio": "tdddd.mp4"
+}
+]
