97312def956c7acab8dd76a68ab59f4b16162bbf
Proladge <proladge@gmail.com>
2025-05-15 13:09:55 +0200

    refactor contuctsus page

--- components/Footer.js
+++ components/Footer.js
@@ -2,11 +2,11 @@
 export function renderFooter(container) {
   const footer = document.createElement('footer');
 
-  debugger;
   footer.innerHTML = `
       <div class="footer-content">
         <p>Створено з любов'ю до мемів у проєкті <strong>Мєм Рація</strong></p>
         <p>🛋 <a href="/pages/contactus.html">Зв'язатись з нами</a></p>
+         <p><a href="/pages/submit" class="submit-link">📤 Надіслати мем</a> <p>
       </div>
     `;
 
