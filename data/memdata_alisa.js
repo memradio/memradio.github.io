0c2fbd603cf12043758800d736c2939c95c5d25e
memradio <volodymyr.y.kovalenko@gmail.com>
2025-05-18 11:03:23 +0200

    додано новий мем: Славка не бери молоду

--- data/memdata_alisa.js
+++ data/memdata_alisa.js
@@ -1,1 +1,11 @@
-[]
+[,
+{
+  "name": "Славка не бери молоду",
+  "links": {
+    "youtube": "",
+    "tiktok": "",
+    "instagram": ""
+  },
+  "audio": "4da0862f-42ae-483d-9b10-c5842972eac1.m4a"
+}
+]
