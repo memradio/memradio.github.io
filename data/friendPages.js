fa5f0fb0ffba8e61590dec50ff1b9cb664d48142
memradio <volodymyr.y.kovalenko@gmail.com>
2025-05-18 10:11:12 +0200

    👥 Додано нового юзера anya до friendPages

--- data/friendPages.js
+++ data/friendPages.js
@@ -2,5 +2,6 @@ export const FRIEND_PAGES = [
   { name: 'kraveculya', emoji: '👩' },
   { name: 'oleg', emoji: '🧔' },
   { name: 'viktor', emoji: '👽' },
-  { name: 'kateryn', emoji: '👩' }
+  { name: 'kateryn', emoji: '👩' },
+  {"name":"anya","emoji":"🐥"}
 ];
