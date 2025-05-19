6bb7cac86b652a5bde3bf01f01767413c0adee25
memradio <volodymyr.y.kovalenko@gmail.com>
2025-05-19 13:07:56 +0200

    👥 Додано нового юзера dima до friendPages

--- data/friendPages.js
+++ data/friendPages.js
@@ -2,5 +2,6 @@ export const FRIEND_PAGES = [
   { name: 'kraveculya', emoji: '👩' },
   { name: 'oleg', emoji: '🧔' },
   { name: 'kateryn', emoji: '👩' },
-  {"name":"alisa","emoji":"💓"}
+  {"name":"alisa","emoji":"💓"},
+  {"name":"dima","emoji":"🏌️"}
 ];
