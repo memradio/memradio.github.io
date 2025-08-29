e01e227f81fcae3be9d0cd868194414af54a62c5
memradio <volodymyr.y.kovalenko@gmail.com>
2025-08-29 23:25:41 +0200

    додано новий мем: Красівий красавчик мой любимий

--- data/memdata_alya.js
+++ data/memdata_alya.js
@@ -1,1 +1,12 @@
-export const MEME_DATA = []
+export const MEME_DATA = [,
+{
+  "number": "001",
+  "name": "Красівий красавчик мой любимий",
+  "links": {
+    "youtube": "",
+    "tiktok": "https://www.tiktok.com/@ha_mi3_1/video/7481959441101999368",
+    "instagram": ""
+  },
+  "audio": "Красівий красавчик мой любимий.mp3"
+}
+]
